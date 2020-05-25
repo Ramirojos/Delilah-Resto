a134ba87620cc159b70707272a034be4b1f4862a
Ramiro <rmarugo@gmail.com>
2020-05-25 18:41:11 -0300

    La taba de usuarios fue creada, se puede hacr el query de SELECT, muestra por consola el contenido de la tabla. pero no en postman. Problema con el res.status.json.

--- rutas/users.js
+++ rutas/users.js
@@ -3,8 +3,9 @@ const jsw = require('jsonwebtoken');
 const router=server.Router();
 const db=require('../config/database');
 const secret ='ulraSecret123';
-const User = require('../models/user')
-/*let users = [
+const User = require('../models/user');
+
+let users = [
     {
         id:1,
         displayName:"Cosme_Fulanito",
@@ -23,17 +24,29 @@ const User = require('../models/user')
         contactPhone:12525545325,
         contactAdress:"calle falsa 123"
     }
-]*/
-router.get('/', (req,res)=>{
+]
 
-User.findAll()
+//https://sequelize.org/master/manual/raw-queries.html
+
+
+//Trae todos los usuarios
+router.get('/', (req,res)=>{
+    db.query('SELECT * FROM users',
+        {type: db.QueryTypes.SELECT}
+        ).then((resultados)=>{
+            console.log(resultados)
+        })
+        .catch(err=>console.log(err));
+
+    /*User.findAll()
     .then(users=>{
         console.log(users);
         res.sendStatus(200);
     })
-    .catch(err=>console.log(err))
+    .catch(err=>console.log(err))*/
 })
 
+// Validacion  de ID usuario 
 function validateUserId(req, res, next){
     users.forEach((user)=>{
         if(user.id == req.params.id){
@@ -44,10 +57,26 @@ function validateUserId(req, res, next){
     })
 }
 
+//Trae un usuario por ID
 router.get('/:id',validateUserId, (req,res)=>{
-    let userId = req.params.id
-    res.status(200).json(users[userId-1]);
-})
+    db.query('SELECT * FROM users WHERE id = ?', {
+        replacements:[req.params.id], type: db.QueryTypes.SELECT})
+        .then(user=>{
+            console.log(user);
+            res.status(200);
+
+        //si pongo res.status(200).json(user),
+        //error cannot set headers after they are sent to the client 
+        //linea 66
+        
+        })
+        .catch(err=>console.log(err));
+    })
+    //let userId = req.params.id
+    
+
+
+//Valida que todos los campos esten completos para la creacion de usuario
 
 function validateNewUser(req,res,next){
     const {id,displayName,password,fullName,email,contactPhone,contactAdress}=req.body;
@@ -58,6 +87,8 @@ function validateNewUser(req,res,next){
     }
 }
 
+
+//Chequea si el DisplayName existe en la creacion de user nuevo
 function checkForUser(req, res, next){
     let existe = false;
     users.forEach((user)=>{
@@ -71,12 +102,21 @@ function checkForUser(req, res, next){
     }    
 }
 
+
+//crea usuario nuevo y agrea a la tabla de usuarios
 router.post('/', [validateNewUser,checkForUser], (req,res)=>{
     //ahora es un push para el arreglo hardcodeado, despues con sequelize
-    users.push(req.body);
+    //users.push(req.body);
+
+
     res.status(201).json('Usuario creado con Exito');
 })
 
+
+//add a user
+
+
+//Chequea qu eel usuari y la contraseña sean correctas cuando quiere lohearse el usuario
 function validateUser ( displayName, password){
     const [filterUser]=users.filter(row=>row.displayName==displayName && row.password==password);
     if(!filterUser){
@@ -85,6 +125,7 @@ function validateUser ( displayName, password){
     return filterUser;
 }
 
+//post request para loguear a la usuario con sus credenciales (si en orden)
 router.post('/:id/login', (req,res)=>{
     const{displayName, password}=req.body;
     const validated = validateUser(displayName,password);
